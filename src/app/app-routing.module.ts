3ff011c2907c1e1ef27705c69b29f05ddab365fb
krstem <krste.moskov@sourcico.com>
2024-05-30 12:18:31 +0200

    fix (implementation) store

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,13 @@ export const routes: Routes = [
     })
   },
   {
+    path: 'login',
+    loadComponent: () => import('./login/login.component').then(m => m.LoginComponent).catch(ex => {
+      console.log(ex.toString(), 'EX LoginComponent');
+      return ex;
+    })
+  },
+  {
     path: 'profile',
     loadComponent: () => import('./profile/profile.component').then(m => m.ProfileComponent).catch(ex => {
       console.log(ex.toString(), 'EX ProfileComponent');
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import {ImageLoaderService} from '../image-loader.service';
 import {MatCardModule} from "@angular/material/card";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
@@ -9,8 +8,9 @@ import {HttpClientModule} from "@angular/common/http";
 import {CommonModule} from "@angular/common";
 import {Store} from "@ngrx/store";
 import {Router} from "@angular/router";
-import {Subject, takeUntil} from "rxjs";
+import {Subject} from "rxjs";
 import {loginRequestAction} from "../store/app.actions";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-login',
@@ -31,34 +31,21 @@ import {loginRequestAction} from "../store/app.actions";
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   loginForm: FormGroup;
-  logoUrl: string = '';
-  unsubscribe = new Subject();
 
-  constructor(private formBuilder: FormBuilder, private imageLoader: ImageLoaderService, private _store: Store<any>, private _route: Router) {
+  constructor(private formBuilder: FormBuilder, private _store: Store<any>, private _route: Router) {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
-    this._store.select("auth", "isAuth").pipe(takeUntil(this.unsubscribe)).subscribe((data: any) => {
+    this._store.select("auth", "isAuth").pipe(takeUntilDestroyed()).subscribe((data: any) => {
       if (data) {
         this._route.navigate(['/profile'])
       } else {
-        this._route.navigate(['/'])
+        this._route.navigate(['/login'])
       }
     })
-  }
-
-  ngOnInit(): void {
-    // this.loadLogo();
-  }
-
-  loadLogo() {
-    const logoUrl = 'http://www.maclocks.eu/media/wysiwyg/Tabit_2.jpg';
-    this.imageLoader.loadImage(logoUrl).subscribe((blob: Blob) => {
-      this.logoUrl = URL.createObjectURL(blob);
-    });
   }
 
   onSubmit(value: any): void {
